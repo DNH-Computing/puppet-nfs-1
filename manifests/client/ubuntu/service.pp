208d6df65004499cae80db8c778bbdf370e2ef20
Daniel Hodder <danielhodder@gmail.com>
2016-07-10 20:50:04 -0700

    Added workaround for xenial not using idmapd

--- manifests/client/ubuntu/service.pp
+++ manifests/client/ubuntu/service.pp
@@ -6,16 +6,19 @@ class nfs::client::ubuntu::service {
     hasstatus => false,
   }
 
-  if $nfs::client::ubuntu::nfs_v4 {
-    service { 'idmapd':
-      ensure    => running,
-      enable    => true,
-      subscribe => Augeas['/etc/idmapd.conf', '/etc/default/nfs-common'],
-    }
-  } else {
-    service { 'idmapd':
-      ensure => stopped,
-      enable => false,
+  # This should probably be less than something but xenial seems to be doing strange things
+  if $lsbdistcodename != 'xenial' {
+    if $nfs::client::ubuntu::nfs_v4 {
+      service { 'idmapd':
+        ensure    => running,
+        enable    => true,
+        subscribe => Augeas['/etc/idmapd.conf', '/etc/default/nfs-common'],
+      }
+    } else {
+      service { 'idmapd':
+        ensure => stopped,
+        enable => false,
+      }
     }
   }
 }
